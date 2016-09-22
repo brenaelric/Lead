d42cfee26a724c3ae66db725c15ff2c78c0af6cd
brena <brenakesia6@gmail.com>
2016-09-22 16:09:41 -0300

    aula 07 sobre documentação

---
+++ Aula07/src/Operacoes.java
@@ -0,0 +1,50 @@
+
+/**
+ * Classe das operações básicas de matemática
+ * 
+ * @author brena
+ * @version 1.0
+ */
+public class Operacoes {
+	/**
+	 * 
+	 * @param a - primeiro valor recebido
+	 * @param b - segundo valor recebido
+	 * @return retorna a soma dos dois parâmetros recebidos
+	 */
+	public double Somar(double a, double b) {
+		return a + b;
+	}
+
+	/**
+	 * 
+	 * @param a - primeiro valor recebido
+	 * @param b - segundo valor recebido
+	 * @return retorna a subtração dos dois parâmtros recebidos. Sendo 'a' p
+	 *         minuendo e 'b' p subtraendo.
+	 */
+	public double Subtrair(double a, double b) {
+		return a - b;
+	}
+
+	/**
+	 * 
+	 * @param a - primeiro valor recebido
+	 * @param b - segundo valor recebido
+	 * @return retorna a multiplicação dos dois parâmetros recebidos
+	 */
+	public double Multiplicar(double a, double b) {
+		return a * b;
+	}
+
+	/**
+	 * 
+	 * @param a - primeiro valor recebido
+	 * @param b - segundo valor recebido
+	 * @return retorna a divisão dos dois parâmetros recebidos, sendo 'a' o
+	 *         dividendo e 'b' o divisor
+	 */
+	public double Dividir(double a, double b) {
+		return a / b;
+	}
+}
