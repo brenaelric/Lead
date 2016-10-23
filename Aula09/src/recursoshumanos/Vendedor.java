bd837fd0161f1f81e317f741cab2b5b91ad55817
brena <brenakesia6@gmail.com>
2016-10-23 15:49:37 -0300

    aula 09

---
+++ Aula09/src/recursoshumanos/Vendedor.java
@@ -0,0 +1,15 @@
+package recursoshumanos;
+
+public class Vendedor extends Funcionario{
+	double comissao;
+	public Vendedor(long cpf, double salario, double desconto, double comissao){
+		super(cpf, salario, desconto);
+		this.comissao = comissao;
+	}
+	public Vendedor(){
+		
+	}
+	double calcularSalario(){
+		return (salario - desconto)+ comissao;
+	}
+}
